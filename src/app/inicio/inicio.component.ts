54e5e1dbefbb6b724bdbb4c66bf45b6dff7eea74
Marcelo <marceloabreu1997@gmail.com>
2022-04-09 22:11:40 -0300

    @m-arceloabreu: task: [Post comentario complete]

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
@@ -20,7 +21,9 @@ export class InicioComponent implements OnInit {
 
     listaPostagem: Postagem[]
     listaComentario: Comentario[]
+    listaPostagemComentario: Postagem
 
+    
     sensivel: boolean
     idUser = environment.id
     nome = environment.nome
@@ -58,6 +61,7 @@ export class InicioComponent implements OnInit {
   getPostagemById(id: number) {
     this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem) => {
       this.postagem = resp
+      console.log(id)
     })
   }
 
@@ -98,7 +102,10 @@ export class InicioComponent implements OnInit {
   getComentarioById(id: number) {
     this.comentarioService.getByIdComentario(id).subscribe((resp: Comentario) => {
       this.comentario = resp
+      
     })
+    
+    
   }
 
   findAllComentarios() {
@@ -107,17 +114,20 @@ export class InicioComponent implements OnInit {
     })
   }
 
-  cadastrarComentario() {
+  cadastrarComentario(id:number){
     this.comentario.sensivel = this.sensivel
+    this.postagem = new Postagem()
+    this.postagem.idPostagem = id
+    this.comentario.postagem = this.postagem
 
     this.usuario.id = this.idUser
     this.comentario.usuario = this.usuario
-
+    
     this.comentarioService.postComentario(this.comentario).subscribe((resp: Comentario) => {
       this.comentario = resp
-      alert('Comentário feito com sucesso!')
+      alert(this.comentario)
       this.comentario = new Comentario()
-      this.findAllComentarios()
+      this.findAllPostagens()
       
     })
   }
