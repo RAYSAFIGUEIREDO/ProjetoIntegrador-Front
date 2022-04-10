d2945c7c478124cc6a9c80cc0b3625b2b22f55c2
Marcelo <marceloabreu1997@gmail.com>
2022-04-10 18:19:02 -0300

    tela about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
-import { SobreComponent } from './sobre/sobre.component';
+
 
 const routes: Routes = [
-  {path: "", redirectTo: "login", pathMatch: "full"},
+  {path: "", redirectTo: "about", pathMatch: "full"},
+  {path: "about", component: AboutComponent},
   {path: "login", component: LoginComponent},
   {path: "cadastrar", component: CadastroComponent},
-  {path: "inicio", component: InicioComponent},
-  {path: "sobre", component: SobreComponent},
+  {path: "inicio", component: InicioComponent}
+  
+
 ];
 
 @NgModule({
